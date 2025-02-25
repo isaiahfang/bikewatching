ebb7261e6a72ad40929942c8f2e7539a979a9cd3
isaiahfang <ifang@ucsd.edu>
2025-02-25 12:51:03 -0800

    Up to 3.2

---
+++ map.js
@@ -0,0 +1,52 @@
+// Import Mapbox as an ESM module
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+// Check that Mapbox GL JS is loaded
+console.log("Mapbox GL JS Loaded:", mapboxgl);
+
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoiaWZhbmciLCJhIjoiY203anhzaXo4MGQwaDJscHhzMHEzcmVieSJ9.W_xHLPtfCxXNK9PhHZ5qzw';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+    container: 'map', // ID of the div where the map will render
+    style: 'mapbox://styles/mapbox/streets-v12', // Map style
+    center: [-71.09415, 42.36027], // [longitude, latitude]
+    zoom: 12, // Initial zoom level
+    minZoom: 5, // Minimum allowed zoom
+    maxZoom: 18 // Maximum allowed zoom
+});
+
+map.on('load', async () => { 
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
+    });
+
+    map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+            'line-color': '#32D400',  // A bright green using hex code
+            'line-width': 3,          // Thicker lines
+            'line-opacity': 0.4       // Slightly less transparent
+        }
+    });
+
+    let jsonData;
+    try {
+        const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
+        
+        // Await JSON fetch
+        const jsonData = await d3.json(jsonurl);
+        
+        console.log('Loaded JSON Data:', jsonData); // Log to verify structure
+        
+        let stations = jsonData.data.stations;
+        console.log('Stations Array:', stations);
+    } catch (error) {
+        console.error('Error loading JSON:', error); // Handle errors
+    }
+});
